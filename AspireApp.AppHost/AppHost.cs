3d41f33a79509907bb79d84b260ebfc6518211ac
Erik Brendbekken Rudlang <erudlang@gmail.com>
2026-05-15 11:54:12 +0200

    add aspire app

---
+++ AspireApp.AppHost/AppHost.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var aks = builder.AddAzureKubernetesEnvironment("aks");
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
